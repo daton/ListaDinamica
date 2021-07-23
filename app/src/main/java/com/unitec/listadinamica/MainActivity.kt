b8e9b6e3f7d2c4c5ad1cc8c0bf37203e4279a86c
daton <rapidclimate@gmail.com>
2021-07-23 18:55:59 -0500

    Initial commit

---
+++ app/src/main/java/com/unitec/listadinamica/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.unitec.listadinamica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /* Aqui iria en la de localiza amigos un servicio REST que nos invocara
+        //(callback) de los usuarios del back end, pero ahora simplemente creamos unos
+        usuario fake.
+         */
+        var u1=Usuario()
+        u1.email="primero@gmail.com"
+        u1.nombre="Juan"
+        u1.edad=21
+
+        var u2=Usuario()
+        u2.email="segundo@gmail.com"
+        u2.nombre="Ana"
+        u2.edad=19
+
+        var u3=Usuario()
+        u3.email="tercera@gmail.com"
+        u3.nombre="Pedro"
+        u3.edad=23
+        //Generamos la lista dinamica fake
+        var usuarios= arrayListOf<Usuario>(u1,u2,u3)
+
+        val usuarioAdapter=UsuarioAdapter{usuario ->adapterOnClick(usuario)  }
+
+        //INvocamos el recuclerView
+        val recyclerView:RecyclerView=findViewById(R.id.recycler_view)
+        //Inyectamos el recycler
+        recyclerView.adapter=usuarioAdapter
+
+        usuarioAdapter.submitList(usuarios)
+
+
+    }
+   //Finalmente agregamos el metodo adapterOnClick, el cual aciva el onClick de cada
+   fun adapterOnClick(usuario:Usuario) {
+       //Aqui tendriamos que inyectarle ora vista para que nos lleve a esa vista
+       //  var intent=Intent(this, OtraActivityQueNoHemosCreado::class.java)
+       //intent.putExtra("email",usuario.email)
+       //startActivity(intent)
+      Toast.makeText(this, usuario.email, Toast.LENGTH_LONG).show()
+   }
+
+}
---
+++ app/src/main/java/com/unitec/listadinamica/Usuario.kt
@@ -0,0 +1,7 @@
+package com.unitec.listadinamica
+
+class Usuario {
+    var nombre:String?=null
+    var email:String?=null
+    var edad:Int?=null
+}
